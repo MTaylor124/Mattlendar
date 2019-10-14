0a35a3f66248cc7035293da0fa601ceebca827f0
MTaylor124 <matthew_taylor1@student.uml.edu>
2019-10-14 16:22:29 -0400

    Added API key and axios get request

--- App.js
+++ App.js
@@ -1,13 +1,38 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, StatusBar } from 'react-native';
 import DayCard from './components/DayCard'
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.title}>MATTLENDAR</Text>
-      <DayCard/>
-    </View>
-  );
+import axios from 'axios'
+const DEFAULT = "02101"
+
+const API_KEY = "d2cf1542594e6c200fc6460bbe090913"
+
+export default class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      zipcode: DEFAULT,
+      days: [],
+
+    }
+  }
+  getForecast(zip) {
+    const request_url = ("api.openweathermap.org/data/weather?zip=" + zip + "us&appid=" + API_KEY + ".json")
+    axios.get(request_url).then((response) => {
+      
+    })
+  }
+  render() {
+    if ( this.state.days.length < 1) {
+      this.getForecast(this.state.zipcode)
+    }
+    return (
+      <View style={styles.container}>
+        <StatusBar hidden/>
+        <Text style={styles.title}>MATTLENDAR FORECAST</Text>
+        <DayCard/>
+      </View>
+    )
+  }
 }
 
 const styles = StyleSheet.create({
--- components/DayCard.js
+++ components/DayCard.js
@@ -1,30 +1,42 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import axios from 'axios'
 
 export default function DayCard () {
   return (
     <View style={styles.card}>
-      <Text style={styles.weekday}>Monday</Text>
+      <Text style={styles.location}>Mansfield, MA</Text>
+      <Text style={styles.weekday}>MONDAY</Text>
       <Text style={styles.date}>1/1/19</Text>
 
         <View style={styles.weatherpic}><Text styles={styles.weatherPicText}>sunny</Text></View>
-      <Text style={styles.temp}>High: 420</Text>
-      <Text style={styles.temp}>Low: 69</Text>
+      <View style={styles.temps}>
+        <Text style={styles.temp}>High: 420</Text>
+        <Text style={styles.temp}>Low: 69</Text>
+      </View>
     </View>
   )
 }
 
 const styles = StyleSheet.create({
+  location: {
+    fontSize: 25
+  },
   date: {
     paddingBottom: 20,
     fontSize: 20
   },
   temp: {
-    fontSize: 35
+    fontSize: 35,
+    padding: 20
+  },
+  temps: {
+    flexDirection: 'row'
   },
   weekday: {
     fontSize: 50,
-    paddingBottom: 10
+    paddingBottom: 10,
+    fontWeight: "600"
   },
   card: {
     flex: 1,
@@ -33,9 +45,10 @@ const styles = StyleSheet.create({
     justifyContent: 'flex-start',
     borderColor: 'black',
     borderWidth: 3,
-    paddingVertical: 30,
-    marginVertical: 20,
-    paddingHorizontal: 55,
+    paddingTop: 20,
+    marginTop: 10,
+    marginBottom: 80,
+    paddingHorizontal: 15,
     borderRadius: 30
   },
   weatherpic: {
